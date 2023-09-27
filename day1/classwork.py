4e1440b6c8a9f1856c19bd7a1c51173282a81c2b
RatiMurgulia <rati.murgulia7@gmail.com>
2023-09-27 13:23:24 -0700

    1commit

--- day1/classwork.py
+++ day1/classwork.py
@@ -1,4 +1,11 @@
-print("hello world")
+mothers_age = 45
+fathers_age = 49
+sisters_age = 17
+
+mothers_age += 5
+fathers_age += 5
+sisters_age += 5
+print(mothers_age,fathers_age,sisters_age)
 
 # github-ზე ატვირთვა
 # git add .
