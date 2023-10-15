917a53c383272c96a3725a539643443ac1a3612f
RatiMurgulia <rati.murgulia7@gmail.com>
2023-10-15 10:47:59 -0700

    3

--- day3/homework/homework1.py
+++ day3/homework/homework1.py
@@ -1,3 +1,5 @@
+# მომხმარებელს შემოატანინეთ სახელი გვარი ასაკი და სიმაღლე და დაპრინტეთ წინადადებაში ყველა ეს მონაცემი
+
 my_name = input("Enter your name: ")
 my_surname = input("Enter your surname: ")
 my_age = int(input("Enter your age: "))
@@ -5,8 +7,3 @@ my_height = float(input("Enter your height: "))
 
 print("My name is "+ my_name+",","my surname is " + my_surname+","+"my age is " + 
        str(my_age)+","+"and my height is " + str(my_height))
-
-year = int(input("Enter year: "))
-new_age = year + (my_age)
-
-print("after 25 yers I'm now " + str(new_age) + " years old")
---
+++ day3/homework/homework2.py
@@ -0,0 +1,7 @@
+# მომხმარებელს შეეკითხეთ ასაკი და დაუპრინტეთ 25 წელში რამდენი წლის იქნება
+
+age = int(input("Enter Your Age: "))
+year = int(input("Enter year: "))
+new_age = year + (age)
+
+print("After 25 yers I'm now " + str(new_age) + " years old")
