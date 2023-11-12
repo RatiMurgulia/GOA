a4d797af3a02a767e7ba9ecaeaf6a49faac5e1ac
RatiMurgulia <rati.murgulia7@gmail.com>
2023-11-12 11:03:43 -0800

    15

--- day15/homework/homework1.py
+++ day15/homework/homework1.py
@@ -1,7 +1,6 @@
 # Square(n) Sum
 def square_sum(numbers):
     my_sum = 0
-    num = len(numbers)
-    while num < len(numbers):
-        my_sum += num ** 2
+    for num in numbers:
+        my_sum += (num ** 2)
     return my_sum
